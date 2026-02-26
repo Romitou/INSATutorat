15ae47425aed337181f7a6c54a9d199c93b041eb
romitou <romitou@protonmail.com>
2026-02-26 08:54:57 +0100

    :lock: Abort request for unauthorized users and non-admins

--- middlewares/admin.go
+++ middlewares/admin.go
@@ -2,6 +2,7 @@ package middlewares
 
 import (
 	"errors"
+
 	"github.com/gin-gonic/gin"
 	"github.com/romitou/insatutorat/apierrors"
 	"github.com/romitou/insatutorat/database/models"
@@ -12,17 +13,20 @@ func AdminHandler() gin.HandlerFunc {
 		userInterface, exists := c.Get("user")
 		if !exists {
 			_ = c.Error(apierrors.Unauthorized)
+			c.Abort()
 			return
 		}
 
 		user, ok := userInterface.(models.User)
 		if !ok {
 			_ = c.Error(errors.New("user is not a valid user")) // ne dois jamais se produire
+			c.Abort()
 			return
 		}
 
 		if !user.IsAdmin {
 			_ = c.Error(apierrors.Forbidden)
+			c.Abort()
 			return
 		}
 
